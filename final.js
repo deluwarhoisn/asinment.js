e10f5de651227c188afab6a0a1fdde1a0044b1b3
deluwarhoisn <babuhossen301@gmail.com>
2025-08-05 00:35:00 +0600

    funtion + jogKorarMachin

---
+++ final.js
@@ -0,0 +1,61 @@
+/** Problem -01 ( Divide the Asset ) */
+
+var area = 800;
+
+var value = area / 2;
+console.log(value);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+
+if(money >= 25000){
+    console.log('Laptop')
+}
+else if(money>= 10000){
+    console.log('Cycle')
+}
+else{
+    console.log('Chocolate')
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+
+for(var day= 1; day<=lastDay; day++){
+    if(day % 3 === 0) {
+        console.log(day+'- medicine');
+    }
+    else{
+        console.log(day+'- rest');
+    }
+}
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+
+if (fileName.includes('#')|| fileName.includes('.pdf') || fileName.includes('.docx')){
+    output = 'Store';
+
+}
+else{
+    output = 'Delete';
+}
+console.log(output);
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
+
+var Email = student.name + student.roll + '.' + student.department + '@ph.ac.bd';
+console.log(Email);
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+
+var incrementRate = 5;
+var currentSalary = startingSalary;
+for(var i =0; i < experience; i++){
+    currentSalary = currentSalary * (1 + incrementRate / 100);
+}
+console.log(currentSalary.toFixed(2));
---
+++ function-2.js
@@ -0,0 +1,16 @@
+// function veugkorarmachin(input1,input2){
+//    let sum = input1+input2;
+// //    return input1+input2;
+//    console.log('sum is ='result);
+// }
+
+// let result = veugkorarmachin(5,10)
+
+ 
+ 
+
+function jogkorarmachin(input1,input2){
+    let sum = input1-input2;
+    console.log('sum is ='-sum)
+}
+jogkorarmachin(20,10);
